0f0933df700b211cf9c34b6ac742dae69bd0129b
Christoph Böhmwalder <christoph@boehmwalder.at>
2020-06-23 17:01:58 +0200

    stations: fix name/meta behavior
    See the comment in the code about the discrepancy between "name" and
    "meta". Interestingly, up until a while ago the API actually tolerated
    these wrong requests with only "meta" set. Now, when we send a search
    request with an empty "name", it returns no results (and doesn't even
    look at the "meta" field).

--- stations.go
+++ stations.go
@@ -10,11 +10,45 @@ import (
 const stationsURL = "https://tickets.oebb.at/api/hafas/v1/stations"
 
 type Station struct {
-	Latitude  int    `json:"latitude"`
-	Longitude int    `json:"longitude"`
-	Name      string `json:"name"`
-	Meta      string `json:"meta"`
-	Number    int    `json:"number"`
+	Latitude  int `json:"latitude"`
+	Longitude int `json:"longitude"`
+	// Name in this context is actually either the "name" or the "meta"
+	// field of the station. Meta stands for a station name which is not
+	// actually a single station, but rather a whole city or a group of
+	// stations. If the name is not set in the response, the "meta" field
+	// is used instead. If the name is set, "meta" is ignored (but it is
+	// usually empty anyways).
+	Name string `json:"name"`
+	// Along with the "name or meta" field, we also store here whether or
+	// not this is a meta station (but we don't transfer that information
+	// to JSON, because the API wouldn't be happy with it).
+	Meta   bool `json:"-"`
+	Number int  `json:"number"`
+}
+
+func (s *Station) UnmarshalJSON(data []byte) error {
+	var raw struct {
+		Latitude  int    `json:"latitude"`
+		Longitude int    `json:"longitude"`
+		Name      string `json:"name"`
+		Meta      string `json:"meta"`
+		Number    int    `json:"number"`
+	}
+
+	if err := json.Unmarshal(data, &raw); err != nil {
+		return err
+	}
+
+	s.Latitude = raw.Latitude
+	s.Longitude = raw.Longitude
+	s.Name = raw.Name
+	if s.Name == "" {
+		s.Name = raw.Meta
+	}
+	s.Meta = raw.Meta != ""
+	s.Number = raw.Number
+
+	return nil
 }
 
 func GetStations(name string, a AuthInfo) ([]Station, error) {
